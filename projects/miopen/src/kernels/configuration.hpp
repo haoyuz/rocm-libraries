ed79c68425d6609fcecacb058185df6a54d490a0
Zoltán Lakatos <zoltan.lakatos@streamhpc.com>
2025-10-28 07:27:02 +0000

    format fix

--- projects/miopen/src/kernels/configuration.hpp
+++ projects/miopen/src/kernels/configuration.hpp
@@ -199,11 +199,12 @@ struct proto_config
     static_assert(CHW >= 0, "MIO_BN_CHW should be always >= 0");
 
     static constexpr auto input_type_strategy = MiopenConfig::input_type_strategy;
-    using fp_type                             = typename std::conditional<
-                                    input_type_strategy == type_strategy::fp16 || input_type_strategy == type_strategy::fpmix,
-                                    _Float16,
-                                    typename std::conditional<input_type_strategy == type_strategy::fp32, float, ushort>::
-                                        type>::type;
+
+    using fp_type = typename std::conditional<
+        input_type_strategy == type_strategy::fp16 || input_type_strategy == type_strategy::fpmix,
+        _Float16,
+        typename std::conditional<input_type_strategy == type_strategy::fp32, float, ushort>::
+            type>::type;
     using fp_prec_type  = float;
     using fp_accum_type = float;
     static constexpr double epsilon =
