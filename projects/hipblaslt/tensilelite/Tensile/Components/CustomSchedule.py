96e17862475d25fd15f16a0ffb86cbbbfa09a055
Ali Nouri <anouri@amd.com>
2025-12-15 19:22:27 +0000

    efficiency improved to 79.88%

--- projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
@@ -1557,12 +1557,12 @@ def _get_schedule_224x128x64_16bit(kernel, useLDSTr, TLDS):
     kernel["MfmaInitCVgprs"] = True
     nglshift = nllshift = 0 # vmcnt shift for ngl and nll
     optSchedule = {
-    'SYNC': [[-1, 6, 12, 12, 27,27, 47, 47]], 
-    'LRA0': [[0,1, 2,3,3,4,4]],
+    'SYNC': [[-1, 6, 13, 13, 27,27, 47, 47]], 
+    'LRA0': [[0,1, 2,3,3,4,5]],
     'GRIncA': [[0, 0, 0, 1, 1, 1, 2, 3, 4]],
     'LRB0': [[9, 11,14, 19]],
     'GRIncB': [[ 5, 5, 5,6,6,6,7,8,9]],
-    'GRA': [[12,12,13, 13, 16,16,18,18,20,20, 26,26, 27, 27]], 
+    'GRA': [[13, 13, 16,16,18,18,20,20,23,23, 26,26, 27, 27]], 
     'LRSA': [[26]],
     'LRSB': [[26]],
     'GRB': [[29,29,33,34,38,38,42,42]],
@@ -1575,7 +1575,7 @@ def _get_schedule_224x128x64_16bit(kernel, useLDSTr, TLDS):
 
     syncCode = [
         SWaitCnt(dscnt=3, vlcnt=-1, vscnt=-1, comment="wait for prior local read local write old=0, new=3 newLW=0 newLR=3 for iteration == 0"),
-        SWaitCnt(dscnt=6, vlcnt=-1, vscnt=-1, comment="wait for prior local read local write"),
+        SWaitCnt(dscnt=7, vlcnt=-1, vscnt=-1, comment="wait for prior local read local write"),
         SWaitCnt(dscnt=2, vlcnt=-1, vscnt=-1, comment=""), # This is for GRA
         SBarrier(comment=""),
         SWaitCnt(dscnt=0, vlcnt=10, vscnt=-1, comment="wait for prior local read local write old=0, new=0 newLW=0 newLR=0"),
