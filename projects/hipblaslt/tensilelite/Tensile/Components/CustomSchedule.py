21287ed9b6af4f6668d3ffe6808e4633ed6142ec
Ali Nouri <anouri@amd.com>
2025-12-16 22:54:21 +0000

    Clean up

--- projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
+++ projects/hipblaslt/tensilelite/Tensile/Components/CustomSchedule.py
@@ -1540,20 +1540,8 @@ def _get_schedule_256x208x64_16bit(kernel, useLDSTr, TLDS):
 )
 
 def _get_schedule_224x128x64_16bit(kernel, useLDSTr, TLDS):
-    """
-    Questions: 
-    1. LRA0 and LRA1 are reading from LDSA0 and LDSA1 ? If so we have to keep issuing LWSA after every LRA0 and LRA1 right?
-    2. Why is there GRIncA 3 times in a row ? Shouldn't there be an increment followed by GRA
-    3. The schedule is for 1 SIMD ? 
-    4. The schedule is for 1 ITERATION ? as opposed to SUBITERATION ? 
-        * One subiteraton is 
-    5. Shouldn't we be alternating instructions of LRA0 and LRA1 and hence many LWSA instructions ?
-
-    """
     if not (isTN(kernel) and TLDS):
         return False, None
-
-    print("USING CMS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     kernel["MfmaInitCVgprs"] = True
     nglshift = nllshift = 0 # vmcnt shift for ngl and nll
     optSchedule = {
